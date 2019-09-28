abab2ce3709aa6a5e8bce200bee70f018060550e
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-09-28 13:36:22 -0400

    completed 5 drills

--- drillsAdvancedPSQL.sql
+++ drillsAdvancedPSQL.sql
@@ -1,13 +1,13 @@
 -- How many people work in the Sales department?
--- SELECT 
---     COUNT(e.id)
--- FROM 
---     employee e
---     JOIN 
---     department d
---     ON e.department = d.id
--- WHERE
---     d.dept_name = 'Sales';
+SELECT 
+    COUNT(e.id)
+FROM 
+    employee e
+    JOIN 
+    department d
+    ON e.department = d.id
+WHERE
+    d.dept_name = 'Sales';
 
 
 
@@ -29,9 +29,64 @@ WHERE
 
 
 -- List the names of employees from the Warehouse department that are assigned to the 'Watch paint dry' project.
+SELECT 
+    e.emp_name AS Employee_name,
+    d.dept_name AS Department,
+    p.project_name AS Project_name
+FROM 
+    employee e
+    JOIN 
+    department d
+    ON e.department = d.id
+    JOIN 
+    employee_project ep
+    ON e.id = ep.emp_id
+    JOIN 
+    project p
+    ON p.id = project_id
+WHERE
+    LOWER(d.dept_name) = 'warehouse'
+    AND
+    LOWER(project_name) = 'watch paint dry';
 
 
 
 
 -- Which projects are the Sales department employees assigned to?
+SELECT 
+    e.emp_name AS Employee_name,
+    d.dept_name AS Department,
+    p.project_name AS Project_name
+FROM 
+    employee e
+    JOIN 
+    department d
+    ON e.department = d.id
+    JOIN 
+    employee_project ep
+    ON e.id = ep.emp_id
+    JOIN 
+    project p
+    ON p.id = project_id
+WHERE
+    LOWER(d.dept_name) = 'sales';
+
+
 -- List only the managers that are assigned to the 'Watch paint dry' project.
+SELECT 
+    e.emp_name AS Managers,
+    d.dept_name AS Department,
+    p.project_name AS Project_name
+FROM
+    department d
+    JOIN
+    employee e
+    ON d.manager = e.id
+    JOIN 
+    employee_project ep
+    ON e.id = ep.emp_id
+    JOIN
+    project p
+    ON p.id = ep.project_id
+WHERE
+    LOWER(p.project_name) = 'watch paint dry';
